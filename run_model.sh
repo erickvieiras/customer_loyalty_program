1d09738fdf17c6bb2c7054de8eed1b58d1104a6f
erickvieiras <erickvieiras.ds@gmail.com>
2024-06-18 19:47:46 -0300

    Deleted unnecessary files for project execution

--- run_model.sh
+++
@@ -1,4 +0,0 @@
-data=$date
-path='/home/erickvieira/Documentos/repos/clustering_project'
-path_to_envs='/home/erickvieira/.pyenv/versions/3.11.7/envs/clusteringvenv/bin/'
-$path_to_envs/papermill $path/src/deploy.ipynb $path/reports/deploy_$data.ipynb
