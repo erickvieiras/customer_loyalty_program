9fc62b060947e9219b3748695d7db0ed9490e6cd
erickvieiras <erickvieiras.ds@gmail.com>
2024-06-18 20:52:14 -0300

    Fixed bugs in deployment

--- app/cluster.py
+++ app/cluster.py
@@ -16,18 +16,33 @@ def clustering(df1):
     with open('source/kmeans_model.pkl', 'rb') as kmeans_file:
         kmeans = pickle.load(kmeans_file)
 
+
+    with open('source/gross_revenue_scaler.pkl', 'rb') as scaler_file:
+        gross_revenue_scaler = pickle.load(scaler_file)
+
+    with open('source/recency_days_scaler.pkl', 'rb') as scaler_file:
+        recency_days_scaler = pickle.load(scaler_file)
+
+    with open('source/qtde_products_scaler.pkl', 'rb') as scaler_file:
+        qtde_products_scaler = pickle.load(scaler_file)
+
+    with open('source/frequency_scaler.pkl', 'rb') as scaler_file:
+        frequency_scaler = pickle.load(scaler_file)
+
+    with open('source/qtde_returns_scaler.pkl', 'rb') as scaler_file:
+        qtde_returns_scaler = pickle.load(scaler_file)
+
     # Selecionando as colunas
     df_space = df1.drop(columns=['customer_id'], axis=1).copy()
     cols_selected = ['customer_id', 'gross_revenue', 'recency_days', 'qtde_products', 'frequency', 'qtde_returns']
     df_space = df1[cols_selected].copy()
 
-    mms = MinMaxScaler()
-
-    df_space['gross_revenue'] = mms.fit_transform(df_space[['gross_revenue']])
-    df_space['recency_days'] = mms.fit_transform(df_space[['recency_days']])
-    df_space['qtde_products'] = mms.fit_transform(df_space[['qtde_products']])
-    df_space['frequency'] = mms.fit_transform(df_space[['frequency']])
-    df_space['qtde_returns'] = mms.fit_transform(df_space[['qtde_returns']])
+    # Aplicar os scalers carregados aos dados
+    df_space['gross_revenue'] = gross_revenue_scaler.transform(df_space[['gross_revenue']])
+    df_space['recency_days'] = recency_days_scaler.transform(df_space[['recency_days']])
+    df_space['qtde_products'] = qtde_products_scaler.transform(df_space[['qtde_products']])
+    df_space['frequency'] = frequency_scaler.transform(df_space[['frequency']])
+    df_space['qtde_returns'] = qtde_returns_scaler.transform(df_space[['qtde_returns']])
 
     # Verificar e tratar valores ausentes
     if df_space.isnull().values.any():
@@ -69,23 +84,23 @@ def clustering(df1):
 
     # Média de receita bruta por cluster
     df_avg_gross_revenue = df_profile[['gross_revenue', 'cluster']].groupby('cluster').mean().reset_index()
-    df_cluster = pd.merge(df_cluster, df_avg_gross_revenue, como='inner', on='cluster')
+    df_cluster = pd.merge(df_cluster, df_avg_gross_revenue, how='inner', on='cluster')
 
     # Média de dias desde a última compra por cluster
     df_avg_recency_days = df_profile[['recency_days', 'cluster']].groupby('cluster').mean().reset_index()
-    df_cluster = pd.merge(df_cluster, df_avg_recency_days, como='inner', on='cluster')
+    df_cluster = pd.merge(df_cluster, df_avg_recency_days, how='inner', on='cluster')
 
     # Média de quantidade de produtos por cluster
     df_qtde_products = df_profile[['qtde_products', 'cluster']].groupby('cluster').mean().reset_index()
-    df_cluster = pd.merge(df_cluster, df_qtde_products, como='inner', on='cluster')
+    df_cluster = pd.merge(df_cluster, df_qtde_products, how='inner', on='cluster')
 
     # Média de frequência de compra por cluster
     df_frequency = df_profile[['frequency', 'cluster']].groupby('cluster').mean().reset_index()
-    df_cluster = pd.merge(df_cluster, df_frequency, como='inner', on='cluster')
+    df_cluster = pd.merge(df_cluster, df_frequency, how='inner', on='cluster')
 
     # Média de devoluções por cluster
     df_qtde_returns = df_profile[['qtde_returns', 'cluster']].groupby('cluster').mean().reset_index()
-    df_cluster = pd.merge(df_cluster, df_qtde_returns, como='inner', on='cluster')
+    df_cluster = pd.merge(df_cluster, df_qtde_returns, how='inner', on='cluster')
     df_cluster = df_cluster.sort_values('gross_revenue', ascending=False)
 
     # Ordenação das colunas
